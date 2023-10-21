f89f4f23209db4a71df64efce7c36eab5a005180
lukemcgovern <lmcgover@uvm.edu>
2023-10-21 19:09:55 -0400

    Added home screen & search bar

--- Fleet-Manager/Fleet-Manager/LogsViewController.swift
+++ Fleet-Manager/Fleet-Manager/LogsViewController.swift
@@ -12,22 +12,22 @@ class LogsViewController: UIViewController {
     @IBOutlet weak var timeLogsButton: UIButton!
     @IBOutlet weak var maintLogsButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func time(_ sender: UIButton) {
+        // create time logs
+        print("time")
+    }
+    
+    @IBAction func maintenance(_ sender: UIButton) {
+        // create maintenance logs
+        print("maintenance")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
