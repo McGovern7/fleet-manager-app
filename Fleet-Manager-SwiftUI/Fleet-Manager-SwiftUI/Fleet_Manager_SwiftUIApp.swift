4b1baf210ecac03939806b8de34a4214825bfbbb
mgaetano <mgaetano@uvm.edu>
2023-11-15 11:07:17 -0500

    SwiftUI conversion first commit

---
+++ Fleet-Manager-SwiftUI/Fleet-Manager-SwiftUI/Fleet_Manager_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fleet_Manager_SwiftUIApp.swift
+//  Fleet-Manager-SwiftUI
+//
+//  Created by user246870 on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Fleet_Manager_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
